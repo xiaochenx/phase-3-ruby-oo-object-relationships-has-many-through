33093c05e83dea7c20e680d9ed2a6259e3af464a
Xiaochen Xu <xiaochex@gmail.com>
2021-06-07 12:58:36 -0400

    commit tree 1550a1fa0d517c33aa05bd9a086b43691990db6b

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self        
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tipped = meals.max{|meal_a, meal_b| meal_a.tip <=> meal_b.tip} 
+        best_tipped.customer 
+    end
 
 end
+
